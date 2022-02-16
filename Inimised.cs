92a82d41d6477fe1f51ca339bdee79e3f42bbc48
opilane <opilane@HP-CZC5261P8F>
2022-02-16 10:52:06 +0200

    new

---
+++ Inimised.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace funktsionid
+{
+    class Inimised
+    {
+        static void Main (string []args)
+        {
+            LinkedList<int> alguses = new LinkedList<int>();
+            for (int i = 0; i < 5; i++)
+            {
+                alguses.AddLast(int.Parse(Console.ReadLine()));
+            }
+            LinkedList<int>.Enumerator ahel1 = alguses.GetEnumerator();
+            while(ahel1.MoveNext())
+            {
+                Console.WriteLine(ahel1.Current);
+            }
+            int j = 0;
+            foreach (int item in alguses)
+            {
+                if (j==0)
+                {
+                    
+                }
+                j++;
+            }
+            Console.ReadLine();
+
+            //Dictionary<int,string>opilased= new Dictionary<int, string>(6);
+            //opilased.Add(74589, "Mihail");
+            //opilased.Add(74590, "Aleksandr");
+            //opilased.Add(74591, "Edgar");
+            //opilased.Add(74592, "Mark");
+            //opilased.Add(74593, "Daniil");
+            //opilased.Add(74594, "Alina");
+            //string nimi = opilased[74590];
+            //opilased[74590] = "Aleksander";
+            //foreach (KeyValuePair<int,string> keyValue in opilased)
+            //{
+            //    Console.WriteLine(keyValue.Key + "-" + keyValue.Value);
+            //}
+
+
+            //List<Inimised> inimised = new List<Inimised>();
+            //inimised.Add(new Inimised() { Nimi = "Mati" });
+            //inimised.Add(new Inimised() { Nimi = "Kati" });
+            //inimised.Add(new Inimised() { Nimi = "Mark" });
+            //foreach (Inimised inimine in inimised)
+            //{
+            //    Console.Write(inimine.Nimi + ", ");
+            //}
+        }
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,53 @@ namespace funktsionid
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
-		}
+            ConsoleKeyInfo click;
+            do
+            {
+                int frequency = 1000;
+
+                int duration = 200;
+                Console.WriteLine("Vajuta mingi klahv!!");
+                click = Console.ReadKey(true);
+                Console.Beep(frequency, duration);
+            } while (click.Key != ConsoleKey.Escape);
+            Environment.Exit(0);
+
+            ConsoleKeyInfo click1;
+            do
+            {
+                int frequency = 3400;
+
+                int duration = 200;
+                Console.WriteLine("Vajuta mingi klahv!!");
+                click1 = Console.ReadKey(true);
+                Console.Beep(frequency, duration);
+            } while (click1.Key != ConsoleKey.Escape);
+            Environment.Exit(0);
+
+            ConsoleKeyInfo click2;
+            do
+            {
+                int frequency = 8000;
+
+                int duration = 200;
+                Console.WriteLine("Vajuta mingi klahv!!");
+                click2 = Console.ReadKey(true);
+                Console.Beep(frequency, duration);
+            } while (click2.Key != ConsoleKey.Escape);
+            Environment.Exit(0);
+
+            ConsoleKeyInfo click3;
+            do
+            {
+                int frequency = 6324;
+
+                int duration = 200;
+                Console.WriteLine("Vajuta mingi klahv!!");
+                click3 = Console.ReadKey(true);
+                Console.Beep(frequency, duration);
+            } while (click3.Key != ConsoleKey.Escape);
+            Environment.Exit(0);
+        }
 	}
 }
